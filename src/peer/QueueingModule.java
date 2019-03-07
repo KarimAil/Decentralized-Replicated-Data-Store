ed4f24b15b8205f5785b5b587cfeaa8e1c1f296d
MohammadAhmadMostafa <Mohammad.rady.98@stud.fci-cu.edu.eg>
2019-03-07 20:04:31 +0200

    add Some Updates

--- src/peer/QueueingModule.java
+++ src/peer/QueueingModule.java
@@ -3,21 +3,73 @@ package peer;
 
 import java.util.ArrayDeque;
 import java.util.Queue;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class QueueingModule {
+public class QueueingModule extends Thread{
+
 
     private static Queue<String> q = new ArrayDeque<>();
-    public static void add(String name)
-    {
-        q.add(name);
-    }
-    QueueingModule()
-    {
 
-    }
-    public static void main(String[] args)
+    public QueueingModule(){}
+
+    private void addToQueue(String Data)
     {
-        QueueingModule qq = new QueueingModule();
+        q.add(Data);
     }
+
+
+    @Override
+    public void run() {
+        try {
+            SaveToFile();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+
+
+    public void getFromDataGen(String Data) throws IOException {
+        addToQueue(Data);
+    }
+
+    public void getFromPeers(String Data) throws IOException {
+        addToQueue(Data);
+    }
+
+
+    public void SaveToFile() throws IOException {
+
+        int size = q.size();
+        PrintWriter pw = new PrintWriter(new FileOutputStream (new File("DB.txt"),true ));
+        for(int i = 0 ; i<size ; i++) {
+
+            pw.append(q.poll()+"|");
+        }
+        pw.close();
+    }
+
+
+    public void print () {
+        int size = q.size();
+        for(int i =0 ; i< size; i++) {
+            System.out.println(q.peek());
+        }
+    }
+	    /*public static void main(String[] args) throws IOException
+	    {
+	        QueueingModule qq = new QueueingModule();
+	        qq.addToQueue("A");
+	        qq.addToQueue("B");
+	        qq.addToQueue("C");
+	        qq.addToQueue("D");
+	        qq.addToQueue("E");
+
+
+	        qq.SaveToFile();
+	    }*/
 }
-
